4fdd8bb22494c59e01bce4d9a4a6bfbfd3e214a2
kh-0818 <lkh3179@naver.com>
2024-08-11 13:20:55 +0900

    1463번 코드,코멘트 추가

--- BOJ/src/p1463/Main.java
+++ BOJ/src/p1463/Main.java
@@ -1,4 +1,25 @@
 package p1463;
 
+import java.util.Scanner;
+
 public class Main {
+    static int input;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        input=sc.nextInt();
+        int count=0;
+        while (input!=1){
+            if(input%3==0){
+                input=input/3;
+                count++;
+            }else if(input%2==0){
+                input=input/2;
+                count++;
+            } else{
+                input-=1;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
