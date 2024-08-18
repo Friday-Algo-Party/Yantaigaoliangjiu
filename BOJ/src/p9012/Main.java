54860f7963e0773c308d4338b1ddddf53dff99cb
kh-0818 <lkh3179@naver.com>
2024-08-19 02:02:30 +0900

    p9012 ì추가

---
+++ BOJ/src/p9012/Main.java
@@ -0,0 +1,61 @@
+package p9012;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            int check=0;
+            boolean isValid = true;
+            String test=br.readLine();
+            char[] input=test.toCharArray();
+            for(char c:input){
+                if(c=='('){
+                    check+=1;
+                }else if(c==')'){
+                    check-=1;
+                }
+                if(check<0){
+                    isValid=false;
+                    break;
+                }
+            }
+            if(isValid&&check==0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//        int n=Integer.parseInt(br.readLine());
+//        for(int i=0;i<n;i++){
+//            int check=0;
+//            String test=br.readLine();
+//            char[] input=test.toCharArray();
+//            for(char c:input){
+//                if(c=='('){
+//                    check+=1;
+//                }else if(c==')'){
+//                    check-=1;
+//                }
+//                if(check<0){
+//                    System.out.println("NO");
+//                    break;
+//                }
+//            }
+//            if(check==0){
+//                System.out.println("YES");
+//            }else{
+//                System.out.println("NO");
+//            }
+//        }
+//    }
+//}
