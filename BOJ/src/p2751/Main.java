fbe44a8a6c48aeb8979fc189bb4b2eed20c7cdae
kh-0818 <lkh3179@naver.com>
2024-09-03 16:38:50 +0900

    2751번 코드 및 정리

---
+++ BOJ/src/p2751/Main.java
@@ -0,0 +1,24 @@
+package p2751;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int []lineup=new int[n];
+        for(int i=0;i<n;i++){
+            lineup[i]=Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(lineup);
+        System.out.println(lineup[0]);
+        for(int i=1;i<n;i++){
+            if(lineup[i]!=lineup[i-1]){
+                System.out.println(lineup[i]);
+            }
+        }
+    }
+}
